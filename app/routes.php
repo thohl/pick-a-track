0229c4b3fa64bf76ca3c26e934a55c88de9e4054
Travis Hohl <thohl@cloud816.com>
2015-01-12 21:50:28 -0600

    Create a new welcome.php view and delete the default hello.php view.

--- app/routes.php
+++ app/routes.php
@@ -13,7 +13,7 @@
 
 Route::get('/', function()
 {
-	return View::make('hello');
+	return View::make('welcome');
 });
 
 Route::get('/users', 'UserController@getIndex');
---
+++ app/views/welcome.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>welcome to pick-a-track.</title>
+	<style>
+		body {
+			margin: 0;
+			font-family: 'Georgia', sans-serif;
+			text-align: center;
+			color: #999;
+		}
+
+		.welcome {
+			width: 300px;
+			height: 200px;
+			margin: 20% auto 0% auto;
+		}
+
+		h1 {
+			font-size: 32px;
+			margin: 16px 0 0 0;
+		}
+	</style>
+</head>
+<body>
+	<div class="welcome">
+		<h1>welcome to pick-a-track.</h1>
+	</div>
+</body>
+</html>
