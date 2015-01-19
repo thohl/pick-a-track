88c9ffbbfc5ba88fc33a1056a2d6e74c214b1a87
Travis Hohl <thohl@cloud816.com>
2015-01-19 08:34:10 -0600

    Create database seed for users table.
    Using Sesame Street characters.

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,27 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
+		$this->call('UserTableSeeder');
+
+		$this->command->info('User table seeded!');
+	}
+
+}
+
+class UserTableSeeder extends Seeder {
+
+	public function run()
+	{
+		Eloquent::unguard();
+		DB::table('users')->delete();
+
+		User::create(array('name' => 'Big Bird',
+											 'email' => 'big.bird@sesamestreet.org'));
+		User::create(array('name' => 'Elmo',
+											 'email' => 'elmo@sesamestreet.org'));
+		User::create(array('name' => 'Cookie Monster',
+											 'email' => 'cookie.monster@sesamestreet.org'));
+
 	}
 
 }
