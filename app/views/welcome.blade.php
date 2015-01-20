8db3e5f3f5d2cc018ef155d353e8683d83c1e388
Travis Hohl <thohl@cloud816.com>
2015-01-20 11:54:11 -0600

    Add 'view users' link to the welcome page.

--- app/views/welcome.blade.php
+++ app/views/welcome.blade.php
@@ -5,6 +5,11 @@
 @stop
 
 @section('style')
+	a {
+		display: block;
+		margin: 25px;
+	}
+
 	body {
 		margin: 0;
 		font-family: 'Georgia', sans-serif;
@@ -12,20 +17,21 @@
 		color: #999;
 	}
 
+	h1 {
+		font-size: 32px;
+		margin: 16px 0 0 0;
+	}
+
 	.welcome {
 		width: 300px;
 		height: 200px;
 		margin: 20% auto 0% auto;
-	}
-
-	h1 {
-		font-size: 32px;
-		margin: 16px 0 0 0;
 	}
 @stop
 
 @section('content')
 	<div class="welcome">
 		<h1>welcome to pick-a-track.</h1>
+		<a href="/users">view users</a>
 	</div>
 @stop
